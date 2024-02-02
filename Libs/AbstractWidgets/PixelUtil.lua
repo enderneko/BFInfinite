75301a368b135eb3fec6ae0f8c7400e49e89a38f
enderneko <enderneko-dev@outlook.com>
2024-02-02 16:56:55 +0800

    Add SetPointOnePixelInner and SetPointOnePixelOuter

--- Libs/AbstractWidgets/PixelUtil.lua
+++ Libs/AbstractWidgets/PixelUtil.lua
@@ -142,6 +142,16 @@ function AW.SetPoint(region, ...)
     region:SetPoint(points[1], points[2], points[3], AW.GetNearestPixelSize(points[4], region:GetEffectiveScale()), AW.GetNearestPixelSize(points[5], region:GetEffectiveScale()))
 end
 
+function AW.SetPointOnePixelInner(region, relativeTo)
+    AW.SetPoint(region, "TOPLEFT", relativeTo, "TOPLEFT", 1, -1)
+    AW.SetPoint(region, "BOTTOMRIGHT", relativeTo, "BOTTOMRIGHT", -1, 1)
+end
+
+function AW.SetPointOnePixelOuter(region, relativeTo)
+    AW.SetPoint(region, "TOPLEFT", relativeTo, "TOPLEFT", -1, 1)
+    AW.SetPoint(region, "BOTTOMRIGHT", relativeTo, "BOTTOMRIGHT", 1, -1)
+end
+
 function AW.ClearPoints(region)
     region:ClearAllPoints()
     if region._points then wipe(region._points) end
