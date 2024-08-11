58e6793ba2e2ec9e317b00f1a30c66b1b0ad177c
enderneko <enderneko-dev@outlook.com>
2024-08-11 18:22:15 +0800

    Update Mouseover/TargetHighlight

--- Modules/UnitFrames/Indicators/MouseoverHighlight.lua
+++ Modules/UnitFrames/Indicators/MouseoverHighlight.lua
@@ -31,22 +31,40 @@ local function MouseoverHighlight_LoadConfig(self, config)
     AW.SetFrameLevel(self, config.frameLevel, self.root)
 
     if config.size ~= 0 then
-        AW.ClearPoints(self)
+        -- AW.ClearPoints(self)
 
-        -- update point
+        -- -- update point
+        -- if config.size < 0 then
+        --     AW.SetPoint(self, "TOPLEFT")
+        --     AW.SetPoint(self, "BOTTOMRIGHT")
+        -- else
+        --     AW.SetPoint(self, "TOPLEFT", -config.size, config.size)
+        --     AW.SetPoint(self, "BOTTOMRIGHT", config.size, -config.size)
+        -- end
+
+        -- -- update thickness
+        -- AW.SetDefaultBackdrop_NoBackground(self, abs(config.size))
+
+        -- -- update color
+        -- self:SetBackdropBorderColor(AW.UnpackColor(config.color))
+
+        -- update thichness
+        local thickness = abs(config.size)
+        AW.ClearPoints(self.mask)
+        AW.ClearPoints(self.tex)
+
         if config.size < 0 then
-            AW.SetPoint(self, "TOPLEFT")
-            AW.SetPoint(self, "BOTTOMRIGHT")
+            AW.SetPoint(self.mask, "TOPLEFT", thickness, -thickness)
+            AW.SetPoint(self.mask, "BOTTOMRIGHT", -thickness, thickness)
+            self.tex:SetAllPoints()
         else
-            AW.SetPoint(self, "TOPLEFT", -config.size, config.size)
-            AW.SetPoint(self, "BOTTOMRIGHT", config.size, -config.size)
+            AW.SetPoint(self.tex, "TOPLEFT", -thickness, thickness)
+            AW.SetPoint(self.tex, "BOTTOMRIGHT", thickness, -thickness)
+            self.mask:SetAllPoints()
         end
 
-        -- update thickness
-        AW.SetDefaultBackdrop_NoBackground(self, abs(config.size))
-
         -- update color
-        self:SetBackdropBorderColor(AW.UnpackColor(config.color))
+        self.tex:SetVertexColor(AW.UnpackColor(config.color))
     else
         self:Hide()
     end
@@ -56,12 +74,24 @@ end
 -- create
 ---------------------------------------------------------------------
 function UF.CreateMouseoverHighlight(parent, name)
-    local highlight = CreateFrame("Frame", name, parent, "BackdropTemplate")
+    local highlight = CreateFrame("Frame", name, parent)
     highlight.root = parent
     highlight:Hide()
+    highlight:SetAllPoints()
 
     parent:HookScript("OnEnter", MouseoverHighlight_OnEnter)
     parent:HookScript("OnLeave", MouseoverHighlight_OnLeave)
+
+    -- mask
+    local mask = highlight:CreateMaskTexture()
+    highlight.mask = mask
+    mask:SetTexture(AW.GetTexture("Empty", true), "CLAMPTOWHITE","CLAMPTOWHITE")
+
+    -- texture
+    local tex = highlight:CreateTexture(nil, "BORDER")
+    highlight.tex = tex
+    tex:SetTexture(AW.GetTexture("White", true))
+    tex:AddMaskTexture(mask)
 
     -- events
     BFI.AddEventHandler(highlight)
