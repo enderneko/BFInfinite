ed931646c790a71f2af9030ed527b2d0a6ad1fca
enderneko <enderneko-dev@outlook.com>
2024-08-15 16:25:14 +0800

    Update role icons

--- Modules/UnitFrames/Indicators/RoleIcon.lua
+++ Modules/UnitFrames/Indicators/RoleIcon.lua
@@ -12,6 +12,14 @@ local UnitGroupRolesAssigned = UnitGroupRolesAssigned
 ---------------------------------------------------------------------
 -- update
 ---------------------------------------------------------------------
+--! CodePoints -> Unicode -> Decimal
+-- https://onlinetools.com/unicode/convert-code-points-to-unicode
+local GLYPHS = {
+    TANK = "\238\128\130",
+    HEALER = "\238\128\131",
+    DAMAGER = "\238\128\132",
+}
+
 local function RoleIcon_Update(self)
     local unit = self.root.unit
     local role = UnitGroupRolesAssigned(unit)
@@ -19,7 +27,9 @@ local function RoleIcon_Update(self)
     if role == "NONE" or (self.hideDamager and role == "DAMAGER") then
         self:Hide()
     else
-        self.icon:SetTexture(AW.GetTexture(role))
+        -- self.icon:SetTexture(AW.GetTexture(role))
+        self.text:SetText(GLYPHS[role])
+        self.text:SetTextColor(AW.GetColorRGB(role))
         self:Show()
     end
 end
@@ -39,6 +49,7 @@ local function RoleIcon_LoadConfig(self, config)
     AW.SetFrameLevel(self, config.frameLevel, self.root)
     UF.LoadIndicatorPosition(self, config.position)
     AW.SetSize(self, config.width, config.height)
+    self.text:SetFont(AW.GetFont("glyphs3"), config.width, "OUTLINE")
     self.hideDamager = config.hideDamager
 end
 
@@ -51,9 +62,14 @@ function UF.CreateRoleIcon(parent, name)
     frame:Hide()
 
     -- icon
-    local icon = frame:CreateTexture(nil, "ARTWORK")
-    frame.icon = icon
-    icon:SetAllPoints()
+    -- local icon = frame:CreateTexture(nil, "ARTWORK")
+    -- frame.icon = icon
+    -- icon:SetAllPoints()
+
+    -- text
+    local text = frame:CreateFontString(nil, "ARTWORK")
+    frame.text = text
+    text:SetPoint("CENTER")
 
     -- events
     BFI.AddEventHandler(frame)
