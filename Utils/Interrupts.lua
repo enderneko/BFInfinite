166872c15b202ee4d827c580f5421bafaa4d45bc
enderneko <enderneko-dev@outlook.com>
2024-08-27 18:03:01 +0800

    Update interrupt

--- Utils/Interrupts.lua
+++ Utils/Interrupts.lua
@@ -6,35 +6,64 @@ local U = BFI.utils
 local IsSpellKnown = IsSpellKnown
 local GetSpellCooldown = C_Spell.GetSpellCooldown
 
+local function GetGCD()
+    return GetSpellCooldown(61304).duration
+end
+
 local INTERRUPT_SPELLS = {
-    WARRIOR = {},
-    PALADIN = {},
-    HUNTER = {
-        187707, -- 压制
+    -- true: check gcd
+    WARRIOR = {
+        [6552] = false, -- 拳击
     },
-    ROGUE = {},
-    PRIEST = {},
-    DEATHKNIGHT = {},
-    SHAMAN = {},
-    MAGE = {},
-    WARLOCK = {},
-    MONK = {},
-    DRUID = {},
-    DEMONHUNTER = {},
-    EVOKER = {},
+    PALADIN = {
+        [96231] = false, -- 责难
+        [31935] = true, -- 复仇者之盾
+    },
+    HUNTER = {
+        [187707] = false, -- 压制
+    },
+    ROGUE = {
+        [1766] = false, -- 脚踢
+    },
+    PRIEST = {
+        [15487] = false, -- 沉默
+    },
+    DEATHKNIGHT = {
+        [47528] = false, -- 心灵冰冻
+    },
+    SHAMAN = {
+        [57994] = false, -- 风剪
+    },
+    MAGE = {
+        [2139] = false, -- 法术反制
+    },
+    WARLOCK = {
+        [119910] = false, -- 法术封锁
+    },
+    MONK = {
+        [116705] = false, -- 切喉手
+    },
+    DRUID = {
+        [106839] = false, -- 迎头痛击
+        [78675] = false, -- 日光术
+    },
+    DEMONHUNTER = {
+        [183752] = false, -- 瓦解
+    },
+    EVOKER = {
+        [351338] = false, -- 镇压
+    },
 }
 
-local interrupt_spell
+local known_spells = {}
 
 local function SPELLS_CHANGED()
-    local found
-    for _, spell in pairs(INTERRUPT_SPELLS[BFI.vars.playerClass]) do
-        if IsSpellKnown then
-            found = spell
-            break
+    wipe(known_spells)
+    for spell, checkGCD in pairs(INTERRUPT_SPELLS[BFI.vars.playerClass]) do
+        if IsSpellKnown(spell) then
+            known_spells[spell] = checkGCD
         end
     end
-    interrupt_spell = found
 end
 
 local timer
@@ -46,7 +75,10 @@ end
 U:RegisterEvent("SPELLS_CHANGED", DELAYED_SPELLS_CHANGED)
 
 function U.InterruptUsable()
-    if interrupt_spell then
-        return GetSpellCooldown(interrupt_spell).duration == 0
+    for spell, checkGCD in pairs(known_spells) do
+        local cd = GetSpellCooldown(spell).duration
+        if cd == 0 or (checkGCD and cd == GetGCD()) then
+            return true
+        end
     end
 end
