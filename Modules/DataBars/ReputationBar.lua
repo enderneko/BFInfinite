30b8767e8a0f31a6cbf410f74d58f01abe1369dc
enderneko <enderneko-dev@outlook.com>
2024-10-18 10:51:17 +0800

    Update ReputationBar.lua

--- Modules/DataBars/ReputationBar.lua
+++ Modules/DataBars/ReputationBar.lua
@@ -98,7 +98,7 @@ local function UpdateRep(self)
     local nextReactionThreshold = data.nextReactionThreshold
     local currentStanding = data.currentStanding
     local factionID = data.factionID
-    BFI.Debug(name, reaction, factionID, "currentStanding:", currentStanding, "current:", currentReactionThreshold, "next:", nextReactionThreshold)
+    -- BFI.Debug(name, reaction, factionID, "currentStanding:", currentStanding, "current:", currentReactionThreshold, "next:", nextReactionThreshold)
 
     local standingLabel, hasRewardPending
     -- TODO: hasRewardPending
@@ -110,7 +110,7 @@ local function UpdateRep(self)
         currentReactionThreshold = info.reactionThreshold or 0
         nextReactionThreshold = info.nextThreshold
         currentStanding = info.standing or 1
-        BFI.Debug("[friendship]", "currentStanding:", currentStanding, "standingLabel:", standingLabel, "current:", currentReactionThreshold, "next:", nextReactionThreshold)
+        -- BFI.Debug("[friendship]", "currentStanding:", currentStanding, "standingLabel:", standingLabel, "current:", currentReactionThreshold, "next:", nextReactionThreshold)
     end
 
     --! paragon (Legion)
@@ -125,7 +125,7 @@ local function UpdateRep(self)
             currentStanding = current % threshold
             reaction = 9
         end
-        BFI.Debug("[paragon]", "currentStanding:", currentStanding, "standingLabel:", standingLabel, "current:", currentReactionThreshold, "next:", nextReactionThreshold)
+        -- BFI.Debug("[paragon]", "currentStanding:", currentStanding, "standingLabel:", standingLabel, "current:", currentReactionThreshold, "next:", nextReactionThreshold)
     end
 
     --! renown
@@ -136,7 +136,7 @@ local function UpdateRep(self)
         currentReactionThreshold = 0
         nextReactionThreshold = data.renownLevelThreshold
         currentStanding = HasMaximumRenown(factionID) and data.renownLevelThreshold or data.renownReputationEarned or 0
-        BFI.Debug("[renown]", "currentStanding:", currentStanding, "standingLabel:", standingLabel, "current:", currentReactionThreshold, "next:", nextReactionThreshold)
+        -- BFI.Debug("[renown]", "currentStanding:", currentStanding, "standingLabel:", standingLabel, "current:", currentReactionThreshold, "next:", nextReactionThreshold)
     end
 
     -- bar
