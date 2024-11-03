a0d694edf4224f4cf0a8071f0f27c437709ab591
enderneko <enderneko-dev@outlook.com>
2024-11-03 18:30:00 +0800

    [NamePlates] Fix target indicator

--- Modules/Nameplates/Common.lua
+++ Modules/Nameplates/Common.lua
@@ -33,8 +33,8 @@ local efficiencyModeIndicators = {
     healthBar = true,
     nameText = true,
     targetIndicator = true,
+    castBar = true,
     healthText = false,
-    castBar = false,
     rareIndicator = false,
     raidIcon = false,
     classIcon = false,
@@ -42,7 +42,6 @@ local efficiencyModeIndicators = {
     buffs = false,
     crowdControls = false,
     questIndicator = false,
-    targetIndicator = false,
 }
 
 function NP.SetupIndicators(np, config, mode)
